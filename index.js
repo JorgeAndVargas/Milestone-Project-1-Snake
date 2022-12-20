890862708eabec925af120b7d51ddd13fb1d06d7
Jorge Vargas <jorgevargas@jorges-mbp.mynetworksettings.com>
2022-12-20 16:46:18 -0500

    fixed errors (spelling), added functions that allow the snake to move

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var rows = 30;
 var ctx;
 
 //Creating the snake
-var snakeX = tabkeSize * 5;
+var snakeX = tableSize * 5;
 var snakeY = tableSize * 5;
 
 //Creating the velocity of the snake
@@ -23,15 +23,59 @@ var appleY;
 //Game is over
 var gameOver = false;
 
+//When the window loads, the board is created
+window.onload = function() {
+    table = document.getElementById("table");
+    table.width = rows * tableSize;
+    table.height = columns * tableSize;
+    ctx = table.getContext("2d");
+    setInterval(game, 1000/10);
+    }
+
+//placing the apple
+function placeApple() {
+    appleX = Math.floor(Math.random() * rows) * tableSize;
+    appleY = Math.floor(Math.random() * columns) * tableSize;
+    }
+
+//Creating the game
+function game() {
+    drawTable();
+    drawSnake();
+    }
+
 //Drawing the table
 function drawTable() {
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "white";
   ctx.fillRect(0, 0, table.width, table.height);
 }
 
 //Drawing the snake
 function drawSnake() {
-    ctx.fillStyle = "green";
+    ctx.fillStyle="orange";
+    snakeX += velocityX * tableSize;
+    snakeY += velocityY * tableSize;
     ctx.fillRect(snakeX, snakeY, tableSize, tableSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        ctx.fillRect(snakeBody[i][0], snakeBody[i][1], tableSize, tableSize);
     }
-    
+}
+
+//Drawing the apples
+function drawApple() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(appleX, appleY, tableSize, tableSize);
+    }
+
+if (snakeX == appleX && snakeY == appleY) {
+    snakeBody.push([appleX, appleY]);
+    placeApple();
+}
+
+for (let i = snakeBody.length-1; i >= 0; i--) {
+    snakeBody[i] = snakeBody[i-1];
+}
+if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+}
+
