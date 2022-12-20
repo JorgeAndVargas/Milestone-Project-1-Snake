c8a2b3968bc635519ced797dd525fdf037e954f5
Jorge Vargas <jorgevargas@jorges-mbp.mynetworksettings.com>
2022-12-20 18:41:34 -0500

    fixed more errors, snake can now end the game when it hits itself, the game resets when the game is over

--- index.js
+++ index.js
@@ -23,59 +23,120 @@ var appleY;
 //Game is over
 var gameOver = false;
 
-//When the window loads, the board is created
+//When the window loads, the table is created and the apples are placed
 window.onload = function() {
     table = document.getElementById("table");
     table.width = rows * tableSize;
     table.height = columns * tableSize;
     ctx = table.getContext("2d");
     setInterval(game, 1000/10);
-    }
 
-//placing the apple
-function placeApple() {
-    appleX = Math.floor(Math.random() * rows) * tableSize;
-    appleY = Math.floor(Math.random() * columns) * tableSize;
+    placeApple();
+    document.addEventListener("keydown", keyPush);
+
     }
 
 //Creating the game
 function game() {
+    if (gameOver) {
+        return;
+    }
+    
+    //if game is over
+
+    if (snakeX < 0 || snakeX > columns*tableSize || snakeY < 0 || snakeY > rows*tableSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+    
+    //Drawing the table
+    function drawTable() {
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, table.width, table.height);
+    }
+
+    //Drawing the apples
+    function drawApple() {
+        ctx.fillStyle = "red";
+        ctx.fillRect(appleX, appleY, tableSize, tableSize);
+
+        //if the snake eats the apple, the snake grows
+        if (snakeX == appleX && snakeY == appleY) {
+            snakeBody.push([appleX, appleY]);
+            placeApple();
+        }
+        for (let i = snakeBody.length-1; i > 0; i--) {
+            snakeBody[i] = snakeBody[i-1];
+        }
+        if (snakeBody.length) {
+            snakeBody[0] = [snakeX, snakeY];
+        }
+    }
+
+    //Drawing the snake
+    function drawSnake() {
+        ctx.fillStyle = "orange";
+        snakeX += velocityX * tableSize;
+        snakeY += velocityY * tableSize;
+        ctx.fillRect(snakeX, snakeY, tableSize, tableSize);
+        for (let i = 0; i < snakeBody.length; i++) {
+            ctx.fillRect(snakeBody[i][0], snakeBody[i][1], tableSize, tableSize);
+        }
+        
+        //if the snake eats itself, the game is over
+        for (let i = 0; i < snakeBody.length; i++) {
+            if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+                gameOver = true;
+                alert("Game Over");
+            }
+        }
+
+        //if the game ends, the snake is reset
+        if (gameOver) {
+            snakeX = tableSize * 5;
+            snakeY = tableSize * 5;
+            velocityX = 0;
+            velocityY = 0;
+            snakeBody = [];
+            gameOver = false;
+        }
+    }
+
     drawTable();
     drawSnake();
+    drawApple();
+
+    
+}
+
+//Placing the apples 
+function placeApple() {
+    appleX = Math.floor(Math.random() * columns) * tableSize;
+    appleY = Math.floor(Math.random() * rows) * tableSize;
+}
+
+
+//Moving the snake
+function keyPush(event) {
+    switch(event.keyCode) {
+        case 37: //Left
+            velocityX = -1;
+            velocityY = 0;
+            break;
+        case 38: //Up
+            velocityX = 0;
+            velocityY = -1;
+            break;
+        case 39: //Right
+            velocityX = 1;
+            velocityY = 0;
+            break;
+        case 40: //Down
+            velocityX = 0;
+            velocityY = 1;
+            break;
     }
-
-//Drawing the table
-function drawTable() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, table.width, table.height);
 }
 
-//Drawing the snake
-function drawSnake() {
-    ctx.fillStyle="orange";
-    snakeX += velocityX * tableSize;
-    snakeY += velocityY * tableSize;
-    ctx.fillRect(snakeX, snakeY, tableSize, tableSize);
-    for (let i = 0; i < snakeBody.length; i++) {
-        ctx.fillRect(snakeBody[i][0], snakeBody[i][1], tableSize, tableSize);
-    }
-}
 
-//Drawing the apples
-function drawApple() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(appleX, appleY, tableSize, tableSize);
-    }
-
-if (snakeX == appleX && snakeY == appleY) {
-    snakeBody.push([appleX, appleY]);
-    placeApple();
-}
-
-for (let i = snakeBody.length-1; i >= 0; i--) {
-    snakeBody[i] = snakeBody[i-1];
-}
-if (snakeBody.length) {
-    snakeBody[0] = [snakeX, snakeY];
-}
 
